da690e8311cce9600e3e2cc653f347bd4ba6b89b
alpha-neri <ayanneri11@gmail.com>
2025-07-30 13:54:07 +0800

    Add files via upload

---
+++ LifeWood/js/firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+
+// IMPORTANT: This file is listed in .gitignore and will NOT be uploaded to GitHub.
+// The values below are placeholders that will be replaced by the GitHub Action.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCslXY-1HXgdrlg04gzW0JYq3lpaj280x4",
+  authDomain: "lifewood-amaze-123.firebaseapp.com",
+  projectId: "lifewood-amaze-123",
+  storageBucket: "lifewood-amaze-123.firebasestorage.app",
+  messagingSenderId: "199649582061",
+  appId: "1:199649582061:web:3d76ba795ee8dbe66c25e4",
+  measurementId: "G-V3T7B34RDT"
+};
